8f31a0acc8aab1ade723a1398761214341de125c
cekun <cekung@gmail.com>
2019-05-22 23:03:31 +0800

    配置文件

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    configureWebpack:{
+        devServer:{
+            port: 8989,
+            open: true, //自动打开网页
+            before(app){
+                
+            }
+        }
+    }
+}
